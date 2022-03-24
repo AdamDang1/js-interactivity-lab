cbedce865ce742e7a0be207a64c8419d2c0d5669
AdamDang1 <dtdang1@gmail.com>
2022-03-24 12:10:32 -0500

    added both delete & cross off functionality

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 // console.log('hello world!')
 
 function addMovie(event) {
-    event.preventDefault()
-    let inputField = document.querySelector('input')
-    let list = document.querySelector('ul')
-    let movie = document.createElement('li')
-    let movieTitle = document.createElement('span')
-    movieTitle.textContent = inputField.value
-    movie.appendChild(movieTitle)
-    list.appendChild(movie)
+    event.preventDefault();
 
-    inputField.value = ''
+    let inputField = document.querySelector('input');
+
+    let movie = document.createElement('li');
+
+    let movieTitle = document.createElement('span');
+    movieTitle.addEventListener('click', crossOffMovie);
+    movieTitle.textContent = inputField.value;
+    movie.appendChild(movieTitle);
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'X'
+    movie.appendChild(deleteBtn);
+    deleteBtn.addEventListener('click', deleteMovie);
+
+    let list = document.querySelector('ul');
+    list.appendChild(movie);
+
+    inputField.value = '';
+}
+
+function deleteMovie(event) {
+    event.target.parentNode.remove();
+}
+
+const crossOffMovie = (event) => {
+    event.target.classList.toggle('checked');
 }
 
 document.querySelector('form').addEventListener('submit', addMovie)
